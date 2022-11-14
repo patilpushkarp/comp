1b17e78b686ff34dcce4f4855ff892138233f742
Pushkar Patil <patilpushkarp@gmail.com>
2022-11-14 11:40:26 +0530

    Updated eda

--- comp/docs/utils/helper.py
+++ comp/docs/utils/helper.py
@@ -4,26 +4,31 @@ import gensim
 import spacy
 nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
 
-def preprocess_tweet(text):
-    """Function to perform preprocessing of tweets"""
-    text = str(text).lower()
-    text = re.sub('@ *\w*', '', str(text))
-    text = re.sub('#\w+', '', str(text))
-    text = re.sub('\n', ' ', str(text))
-    text = re.sub('\xa0', ' ', str(text))
-    text = re.sub('&amp', ' ', str(text))
-    text = re.sub(r'http\S+', '', str(text))
-    text = re.sub(r'[^A-Za-z0-9 ]+', '', str(text))
-    return text
+class Processor:
 
-def sentence_to_words(sentences):
-    """Function to convert sentences to words"""
-    return (gensim.utils.simple_preprocess(str(sentence), deacc=True) for sentence in sentences)
+    def __init__(self):
+        pass
 
-def lemmatization(words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
-    """Function to lemmatized tokenied sentence"""
-    doc = nlp(" ".join(words))
-    return [token.lemma_ for token in doc if token.pos_ in allowed_postags]
+    def preprocess_tweet(self, text):
+        """Function to perform preprocessing of tweets"""
+        text = str(text).lower()
+        text = re.sub('@ *\w*', '', str(text))
+        text = re.sub('#\w+', '', str(text))
+        text = re.sub('\n', ' ', str(text))
+        text = re.sub('\xa0', ' ', str(text))
+        text = re.sub('&amp', ' ', str(text))
+        text = re.sub(r'http\S+', '', str(text))
+        text = re.sub(r'[^A-Za-z0-9 ]+', '', str(text))
+        return text
+
+    def sentence_to_words(sentences):
+        """Function to convert sentences to words"""
+        return (gensim.utils.simple_preprocess(str(sentence), deacc=True) for sentence in sentences)
+
+    def lemmatization(words, allowed_postags=['NOUN', 'ADJ', 'VERB', 'ADV']):
+        """Function to lemmatized tokenied sentence"""
+        doc = nlp(" ".join(words))
+        return [token.lemma_ for token in doc if token.pos_ in allowed_postags]
 
 
 
